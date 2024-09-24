29c0c99c7c71f5ac9841fbe7cca55bb681efac32
Sachin Singh <sachinsingh9272@gmail.com>
2024-09-25 01:23:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Accordian from './components/accordian/index'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Accordian/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/accordian/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        id:'1',
+        question:"What are accordian components?",
+        answer:"Accordian components are user interface elements used for organinizing and presenting"
+    },
+    {
+        id:'2',
+        question:"What are they udes for?",
+        answer:"They are commonly employed in various contexts, including FAQs, product description"
+    },
+    {
+        id:'3',
+        question:"Accordian as musical instrument",
+        answer:"Accordian is a musical instrument with a keyboard and bellows. It produces sound."
+    }
+]
+
+export default data;
